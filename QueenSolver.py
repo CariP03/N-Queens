48a8e15cc5508ab2263043ce3c240bdb28b733e8
CariP03 <pasqualecaringellab@gmail.com>
2025-03-22 18:50:24 +0100

    feat: add QueenSolver
    Add the class QueenSolver that contains all methods to solve the
    N-Queens problem

---
+++ QueenSolver.py
@@ -0,0 +1,53 @@
+class QueenSolver:
+    @staticmethod
+    def __validate_pos(candidate: [int], row: int) -> bool:
+        val = True
+
+        # Check for column threat
+        for i in range(row):
+            if candidate[row] == candidate[i]:
+                val = False
+
+        # Check for diagonal threat
+        if val:
+            for i in range(row):
+                if abs(row - i) == abs(candidate[row] - candidate[i]):
+                    val = False
+
+        return val
+
+    @staticmethod
+    def solve(board_size: int):
+        # Inner recursive function
+        def inner_solve(candidate: [int], row: int) -> ([int], bool):
+            found = False  # initialize found
+
+            # Try to add a queen in each column
+            for j in range(board_size):
+                candidate[row] = j
+
+                # Check if the move is legal
+                if QueenSolver.__validate_pos(candidate, row):
+                    if row != board_size - 1:
+                        candidate, found = inner_solve(candidate, row + 1)  # Try to add a queen in the next row
+
+                    else:
+                        # Return a solution and set the found flag to True
+                        return candidate, True
+
+                # Check if a solution has been found
+                if found:
+                    return candidate, True
+
+            # No solution has been found
+            return candidate, False
+
+        # Initialize the loop
+        empty_solution = [-1] * board_size
+        solution, solved = inner_solve(empty_solution, 0)
+
+        # Return a solution if found
+        if solved:
+            return solution
+        else:
+            return None
--- main.py
+++ main.py
@@ -1,11 +1,7 @@
 import turtle
 
-from BoardPen import *
+import BoardPen
+from QueenSolver import QueenSolver
 
 if __name__ == '__main__':
-    board = BoardPen(8)
-    board.draw_board()
-
-    board.place_queen(7, 7)
-
-    turtle.mainloop()
+    print(QueenSolver.solve(8))
