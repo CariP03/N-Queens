b1fb127fe47fda8a8651c38ce6885c403e94e353
CariP03 <pasqualecaringellab@gmail.com>
2025-03-22 16:39:08 +0100

    feat!: add placeQueen function
    Add placeQueen function which allow to print the symbol of a queen at
    the center of the desired cell.
    
    BREAKING CHANGE: BoardPen is not a static class anymore and it has to be
    instantiated before use.

--- BoardPen.py
+++ BoardPen.py
@@ -1,10 +1,18 @@
 import turtle as t
 
-class BoardPen:
-    # Private attributes
-    __SQUARE_SIZE = 30  # measured in pixels
 
+class BoardPen:
+    # Class attributes
+    __SQUARE_SIZE = 30  # measured in pixels
+    __QUEEN_PATH = "resources/Queen.gif"
+
+    # Class configuration
     t.speed(0)  # set the speed of the turtle to the maximum value
+    t.register_shape(__QUEEN_PATH)  # add the shape of the queen
+
+    # Constructor
+    def __init__(self, board_size: int):
+        self.size = board_size
 
     @staticmethod
     def __draw_square():
@@ -15,31 +23,29 @@ class BoardPen:
 
         t.forward(BoardPen.__SQUARE_SIZE)  # move to the next square
 
-    @staticmethod
-    def __to_next_line(size: int):
+    def __to_next_line(self):
         # Move to the start of the new row
         t.left(90)
         t.forward(BoardPen.__SQUARE_SIZE)
 
         t.left(90)
-        t.forward(size * BoardPen.__SQUARE_SIZE)
+        t.forward(self.size * BoardPen.__SQUARE_SIZE)
 
         t.right(180)
 
-    @staticmethod
-    def draw_board(size: int):
+    def draw_board(self):
         # Avoid animation
         t.tracer(1, 0)
 
         # Set initial position
-        initial_pos = -size * BoardPen.__SQUARE_SIZE / 2
+        initial_pos = -self.size * BoardPen.__SQUARE_SIZE / 2
 
         t.penup()
         t.setpos(initial_pos, initial_pos)
         t.pendown()
 
-        for i in range(size):
-            for j in range(size):
+        for i in range(self.size):
+            for j in range(self.size):
 
                 # Fill square
                 t.begin_fill()
@@ -55,8 +61,27 @@ class BoardPen:
                 t.end_fill()
                 t.update()
 
-            BoardPen.__to_next_line(size)
+            BoardPen.__to_next_line(self)
 
         # End drawing
         t.hideturtle()
-        t.mainloop()
+
+    def __calculate_center(self, row: int, col: int) -> (int, int):
+        initial_pos = -self.size * BoardPen.__SQUARE_SIZE / 2  # leftmost bottom pixel of the board
+
+        x = initial_pos + col * BoardPen.__SQUARE_SIZE + BoardPen.__SQUARE_SIZE / 2
+        y = initial_pos + row * BoardPen.__SQUARE_SIZE + BoardPen.__SQUARE_SIZE / 2
+
+        return x, y
+
+    def place_queen(self, row: int, col: int):
+        t.shape(BoardPen.__QUEEN_PATH)  # set the turtle to draw a queen
+        center = BoardPen.__calculate_center(self, row, col)
+
+        # move to the center
+        t.penup()
+        t.setpos(center)
+        t.pendown()
+
+        # Draw the queen
+        t.stamp()
--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import turtle
+
 from BoardPen import *
 
 if __name__ == '__main__':
-    BoardPen.draw_board(8)
+    board = BoardPen(8)
+    board.draw_board()
 
+    board.place_queen(7, 7)
+
+    turtle.mainloop()
